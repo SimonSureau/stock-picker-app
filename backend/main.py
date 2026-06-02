14229f1b84b02a8637e8f039946a65f6709f3e68
Simon <simonsureau02@gmail.com>
2026-06-02 17:53:22 -0400

    Fix CORS allow all origins

--- backend/main.py
+++ backend/main.py
@@ -11,13 +11,9 @@ app = FastAPI(title="Stock Picker API")
 # Allow your React frontend to talk to this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://stock-picker-app-chi.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
 )
 
 # Route 1: Get a current stock quote
